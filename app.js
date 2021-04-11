7aba309a87533c480610cbeaf5d61da2aedb96a3
Al-aminAzad <alaminazad240@gmail.com>
2021-04-11 19:17:07 +0600

    work with webhooks for better payment

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const bookingRouter = require('./routes/bookingRoutes');
 const AppError = require('./utilities/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const viewRouter = require('./routes/viewRoutes');
+const bookingController = require('./controllers/bookingController');
 
 app.enable('trust proxy');
 app.set('view engine', 'pug');
@@ -52,6 +53,8 @@ const limiter = rateLimit({
   message: 'Too many request from this IP!.Please try again in an hour',
 });
 app.use('/api', limiter);
+
+app.post('/webhook-checkout',express.raw({type:'application/json'}), bookingController.webhookCheckout);
 
 //Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,11 +4,16 @@ const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 const bookingController = require('../controllers/bookingController');
 
-router.get('/', bookingController.createBookingCheckout, authController.isLoggedIn, viewsController.getOverview);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/signup', authController.isLoggedIn, viewsController.getSignupForm);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
-router.get('/my-tours', authController.protect, viewsController.getMyTours);
+router.get(
+  '/my-tours',
+  //  bookingController.createBookingCheckout,
+  authController.protect,
+  viewsController.getMyTours
+);
 // router.post('/submit-user-data',authController.protect, viewsController.updateUserData);
 module.exports = router;
