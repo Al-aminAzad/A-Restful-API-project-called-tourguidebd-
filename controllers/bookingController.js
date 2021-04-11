91b4eccd4df105a7fdaaf3bee393da0dfac37c52
Al-aminAzad <alaminazad240@gmail.com>
2021-04-11 21:49:22 +0600

    some updated

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -10,7 +10,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   //create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${
+    success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${req.params.tourId}&user=${req.user.id}&price=${
       tour.price
     }`,
     // success_url: `${req.protocol}://${req.get('host')}/my-tours`,
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,14 +4,14 @@ const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 const bookingController = require('../controllers/bookingController');
 
-router.get('/',bookingController.createBookingCheckout, authController.isLoggedIn, viewsController.getOverview);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/signup', authController.isLoggedIn, viewsController.getSignupForm);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
 router.get(
   '/my-tours',
-  //  bookingController.createBookingCheckout,
+   bookingController.createBookingCheckout,
   authController.protect,
   viewsController.getMyTours
 );
